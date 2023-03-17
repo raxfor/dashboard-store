83ee61ba64e54d585a98fa3118405386c4d4f515
raxfor <jhonalexslb@gmail.com>
2023-03-17 14:42:17 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="bg-red-400">Hola Mundo</h1>
+    </div>
+  )
+}
+
+export default App
